ef6ebbb297126a116d7c75fcf64c41d75e5b6de9
Piyush Sharma <piyushsharma071999@gmail.com>
2020-10-25 11:08:28 +0530

    Modified Corect Solution

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,8 +1,13 @@
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
+  
+// IIFE to create function that displays "Good Bye" + " " + "name". name will be passed as arguement.
+
+(function(window){
+	var byeSpeaker = {};
+	var speakWord = "Good Bye";
+	byeSpeaker.speak = function speak(name) {
+  		console.log(speakWord + " " + name);
+	}
+
+window.byeSpeaker = byeSpeaker;
+
 })(window);
